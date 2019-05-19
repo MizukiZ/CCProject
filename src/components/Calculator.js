0536358e545e34d505898ed8f122032105619b03
Mizuki Zenta <mizukisb@gmail.com>
2019-05-19 18:32:17 +1000

    added react native easy grid

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,26 +5,39 @@ import { Calculator } from "react-native-calculator"
 import { toggelCalculator } from "../store/actions/index"
 
 import { Container, Button, Icon, Text } from "native-base"
+import { Col, Row, Grid } from "react-native-easy-grid"
 
 class CCCalculator extends Component {
   render() {
     return (
-      <Modal isVisible={this.props.calculator} animationInTiming={800}>
-        <Container>
-          <Calculator
-            style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-            width={200}
-            height={300}
-            calcButtonBackgroundColor="#00e9ff"
-          />
-          <Button iconLeft danger onPress={this.props.onToggleCalculator}>
-            <Icon name="close" />
-            <Text>Close</Text>
-          </Button>
+      <Modal isVisible={this.props.calculator} animationInTiming={600}>
+        <Container style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}>
+          <Grid>
+            <Row size={85}>
+              <Calculator
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+                width={300}
+                height={450}
+                calcButtonBackgroundColor="#00e9ff"
+              />
+            </Row>
+
+            <Row
+              size={15}
+              style={{
+                justifyContent: "center"
+              }}
+            >
+              <Button iconLeft danger onPress={this.props.onToggleCalculator}>
+                <Icon name="close" />
+                <Text>Close</Text>
+              </Button>
+            </Row>
+          </Grid>
         </Container>
       </Modal>
     )
