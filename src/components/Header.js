52f9d88d352360e314f5275f1ff827b5ecf7382c
Mizuki Zenta <mizukisb@gmail.com>
2019-05-24 17:07:54 +1000

    Added base currency flag on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react"
-import { Header, Left, Title, Body, Right } from "native-base"
+import { Header, Left, Title, Body, Right, Thumbnail } from "native-base"
+import { connect } from "react-redux"
 
-const CCHeader = () => {
-  return (
-    <Header>
-      <Left />
-      <Body>
-        <Title>Dashboard</Title>
-      </Body>
-      <Right />
-    </Header>
-  )
+import CountryInfo from "../assets/counrty_Infomation_handler"
+
+class CCHeader extends Component {
+  render() {
+    return (
+      <Header>
+        <Left />
+        <Body>
+          <Title>Dashboard</Title>
+        </Body>
+        <Right>
+          <Thumbnail
+            square
+            source={CountryInfo[this.props.setting.baseCurrency].flag}
+          />
+        </Right>
+      </Header>
+    )
+  }
 }
 
-export default CCHeader
+const mapStateToProps = state => {
+  return {
+    setting: state.setting
+  }
+}
+
+export default connect(mapStateToProps)(CCHeader)
