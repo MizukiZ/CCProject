786196d03b04740b51d6a9cb59d40945f2883d7c
Mizuki Zenta <mizukisb@gmail.com>
2019-06-01 16:16:12 +1000

    added tab activation system

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,42 +6,39 @@ import { toggelCalculator } from "../store/actions/index"
 import { NavigationActions } from "react-navigation"
 
 class CCFooter extends Component {
+  state = {
+    activeSection: "Home"
+  }
   render() {
     return (
       <Footer>
-        <FooterTab>
+        <FooterTab style={{ backgroundColor: "#F9F9F9" }}>
           <Button
+            active={this.state.activeSection == "Home" ? true : false}
             onPress={() => {
+              this.setState({ activeSection: "Home" })
               this.props.onPressIcon("Home")
             }}
           >
-            <Icon
-              type="FontAwesome"
-              name="home"
-              style={{ fontSize: 40, color: "black" }}
-            />
+            <Icon type="FontAwesome" name="home" style={{ fontSize: 40 }} />
           </Button>
           <Button
+            active={this.state.activeSection == "History" ? true : false}
             onPress={() => {
+              this.setState({ activeSection: "History" })
               this.props.onPressIcon("History")
             }}
           >
-            <Icon
-              type="FontAwesome"
-              name="history"
-              style={{ fontSize: 40, color: "black" }}
-            />
+            <Icon type="FontAwesome" name="history" style={{ fontSize: 40 }} />
           </Button>
           <Button
+            active={this.state.activeSection == "Setting" ? true : false}
             onPress={() => {
+              this.setState({ activeSection: "Setting" })
               this.props.onPressIcon("Setting")
             }}
           >
-            <Icon
-              type="FontAwesome"
-              name="cogs"
-              style={{ fontSize: 40, color: "black" }}
-            />
+            <Icon type="FontAwesome" name="cogs" style={{ fontSize: 40 }} />
           </Button>
           <Button onPress={this.props.onToggleCalculator}>
             <Icon
