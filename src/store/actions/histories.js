1498c76b9206cd8bbcac960d3b9d46d9abe187ff
Mizuki Zenta <mizukisb@gmail.com>
2019-06-01 11:57:29 +1000

    chnage database path

--- src/store/actions/histories.js
+++ src/store/actions/histories.js
@@ -1,37 +1,10 @@
 import {
-  CHANGE_AUTO_CONVERTION_HISTORY_SAVE,
-  CHANGE_AUTO_LOCATION,
-  CHANGE_BASE_CURRENCY,
-  FETCH_DEVICE_SETTING,
-  ADD_CURRENCY,
-  DELETE_CURRENCY
+  FETCH_CONVERT_HISTORY,
+  ADD_CONVERT_HISTORY,
+  DELETE_CONVERT_HISTORY
 } from "./actionTypes"
 import DeviceInfo from "react-native-device-info"
 import firebase from "react-native-firebase"
-import { fetchCurrencyLatestData } from "./index"
-
-export const changeAutoConvertionHistorySave = flag => {
-  return {
-    type: CHANGE_AUTO_CONVERTION_HISTORY_SAVE,
-    flag: flag
-  }
-}
-
-export const changeAutoConvertionHistorySaveFromFirebase = flag => {
-  return dispatch => {
-    return firebase
-      .database()
-      .ref(`/${deviceID}/settings/`)
-      .update({ convertionHistorySave: flag })
-      .then(function(snapshot) {
-        // update redux aswell
-        dispatch(changeAutoConvertionHistorySave(flag))
-      })
-      .catch(error => {
-        throw error
-      })
-  }
-}
 
 export const changeAutoLocation = flag => {
   return {
@@ -80,20 +53,21 @@ export const changeBaseCurrencyFromFirebase = currencyCode => {
   }
 }
 
-export const fetchDeviceSettings = settings => {
+export const fetchConvertHistory = data => {
   return {
-    type: FETCH_DEVICE_SETTING,
-    settings: settings
+    type: FETCH_CONVERT_HISTORY,
+    data: data
   }
 }
 
 export const fechDeviceSettingsFromFirebase = () => {
   // get unique device id
   deviceID = DeviceInfo.getUniqueID()
+
   return dispatch => {
     return firebase
       .database()
-      .ref(`/${deviceID}/settings/`)
+      .ref("/history/" + deviceID)
       .once("value")
       .then(function(snapshot) {
         // if there is an exsisting settings for the currenct device
@@ -119,7 +93,7 @@ export const fechDeviceSettingsFromFirebase = () => {
         } else {
           // if there is no setting for the device then create setting with default values
 
-          return firebase
+          firebase
             .database()
             .ref(`/${deviceID}/settings/`)
             .set({
@@ -128,33 +102,6 @@ export const fechDeviceSettingsFromFirebase = () => {
               autoLocation: false,
               displayCurrency: { 0: "JPY" },
               loaded: true
-            })
-            .then(function() {
-              // after initial setting fetch data again
-              return firebase
-                .database()
-                .ref(`/${deviceID}/settings/`)
-                .once("value")
-                .then(function(snapshot) {
-                  // get each value from snapshot
-                  const {
-                    baseCurrency,
-                    convertionHistorySave,
-                    autoLocation,
-                    displayCurrency
-                  } = snapshot.val()
-
-                  // dispatch the mothod to update state
-                  dispatch(
-                    fetchDeviceSettings({
-                      baseCurrency,
-                      convertionHistorySave,
-                      autoLocation,
-                      displayCurrency,
-                      loaded: true
-                    })
-                  )
-                })
             })
         }
       })
