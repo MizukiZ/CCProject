1805371daa303604d57414dc9caa38eb0531e59d
Mizuki Zenta <mizukisb@gmail.com>
2019-06-02 15:41:33 +1000

    implement initial connection check

--- android/app/src/main/java/com/cc_project/MainApplication.java
+++ android/app/src/main/java/com/cc_project/MainApplication.java
@@ -3,6 +3,7 @@ package com.cc_project;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NetInfoPackage(),
             new SvgPackage(),
             new RNGestureHandlerPackage(),
             new RNFirebasePackage(),
